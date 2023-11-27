9068fd8ea3cbe2470f724d38960a982005ad5d3a
Kacper Lewicki <kacper.lewicki@frista.com>
2023-11-27 18:04:05 +0100

    add style for invoicelist and inovice

--- src/components/Invoice/Invoice.jsx
+++ src/components/Invoice/Invoice.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-
+import "./Invoice.css";
 export const Invoice = ({ invoice }) => {
 
-return (
-    <li>
-    <p>Name: {invoice.name}</p>
-    <p>Date: {invoice.date}</p>
-    <p>Amount: {invoice.amount}</p>
-    <p>Status: {invoice.status}</p>
-    <p>Customer name: {invoice.customer_name}</p>
-    <p>Payment due date: {invoice.payment_due_date}</p>
-    <p>Currency: {invoice.currency}</p>
+return (   
+    <li className='invoice_li'>
+    <p className='invoice_p'>Name: {invoice.name}</p>
+    <p className='invoice_p'>Date: {invoice.date}</p>
+    <p className='invoice_p'>Amount: {invoice.amount}</p>
+    <p className='invoice_p'>Currency: {invoice.currency}</p>
+    <p className='invoice_p'>Status: {invoice.status}</p>
+    <p className='invoice_p'>Customer name: {invoice.customer_name}</p>
+    <p className='invoice_p'>Payment due date: {invoice.payment_due_date}</p>
+   
     </li>
+   
     )
 
 }
