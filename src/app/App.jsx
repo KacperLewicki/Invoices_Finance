c5c0dc6e1b76d0c11fd2e93252da06a63debf315
Kacper Lewicki <kacper.lewicki@frista.com>
2023-11-28 18:12:13 +0100

    adding routing to the page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import  InvoiceList from '../components/Invoice_List/InvoiceList';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import  {InvoiceListPage}  from '../components/invoicesPage/InvoicesPage';
+import NewsPage from '../components/newsPage/NewsPage';
 function App() {
-    return (
-        <InvoiceList />
-    )
+  return (   
+    <Router>
+    <Link to="/"><button>Home</button></Link>
+    <Link to="/InvoiceListPage"><button>Invoices List</button></Link> 
+    <Link to="/NewsPage"><button>News Page</button></Link>
+
+    <Routes>
+        <Route path="/InvoiceListPage" element={<InvoiceListPage />} />   
+        <Route path='/NewsPage' element={<NewsPage />}></Route>
+    </Routes>
+    </Router>
+
+)
 }
 
 export default App;
