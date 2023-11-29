955bf2ad0519eadde62dcbf367f412fe95b3619b
Kacper Lewicki <kacper.lewicki@frista.com>
2023-11-29 19:57:06 +0100

    add new style for app_button

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import  {InvoiceListPage}  from '../components/invoicesPage/InvoicesPage';
 import NewsPage from '../components/newsPage/NewsPage';
-function App() {
-  return (   
-    <Router>
-    <Link to="/"><button>Home</button></Link>
-    <Link to="/InvoiceListPage"><button>Invoices List</button></Link> 
-    <Link to="/NewsPage"><button>News Page</button></Link>
+import "./App.css";
 
+function App() {
+
+  return (  
+
+    <Router>
+    <div className='buttons_Menu'>
+    <Link to="/"><button className='button_StartMenu'>Home</button></Link>
+    <Link to="/InvoiceListPage"><button className='button_StartMenu'>Invoices List</button></Link> 
+    <Link to="/NewsPage"><button className='button_StartMenu'>News Page</button></Link>
+    </div>
     <Routes>
         <Route path="/InvoiceListPage" element={<InvoiceListPage />} />   
         <Route path='/NewsPage' element={<NewsPage />}></Route>
--- src/components/newsPage/NewsPage.jsx
+++ src/components/newsPage/NewsPage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import "./NewsPage.css"
 function NewsPage() {
  return (
     <>
-    <h1>Power platform developer</h1>
-    <p> Nowe prace dotyczące power platform od microsoft</p>
+    <h1 className='NewsPage_h1'>Power platform developer</h1>
+    <p className='NewsPage_p'> Nowe prace dotyczące power platform od microsoft</p>
     </>
  )
 }
