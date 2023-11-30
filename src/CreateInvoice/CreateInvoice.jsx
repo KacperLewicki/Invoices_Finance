5490ea631a96eca05d1e333c6bb8404691b6eac2
Kacper Lewicki <kacper.lewicki@frista.com>
2023-11-30 16:43:15 +0100

    new content in app create_Invoice_manual

---
+++ src/CreateInvoice/CreateInvoice.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CreateInvoice extends React.Component {
+
+render(){
+    return(
+        <h1>Create Invoice manual</h1>
+    ) 
+}
+
+
+}
+
+export default CreateInvoice;
