4e223c6e6ca9d8014bcde62db3913bc61a3da8ad
Kacper Lewicki <kacper.lewicki@frista.com>
2023-12-04 10:16:28 +0100

    add new input in createInvoice

--- src/CreateInvoice/CreateInvoice.jsx
+++ src/CreateInvoice/CreateInvoice.jsx
@@ -3,22 +3,33 @@ import React from 'react';
 class CreateInvoice extends React.Component {
    state = {
         nameInvoice: '',
+        dataInvoice: '',
     }
 
-chandleNameInvoice = (event) => {
+chandlerInvoice = (event) => {
         this.setState({nameInvoice: event.target.value});
+        this.setState({dataInvoice: event.target.value});
+        
+        
     }
 
 render(){
     return(
         <>
+        <div>
         <h1>Create Invoice manual</h1>
         <input name='nameInvoice' 
         type='text' 
         value={this.state.nameInvoice}
-        onChange={this.chandleNameInvoice} 
+        onChange={this.chandlerInvoice} 
         placeholder='nazwa faktury'>
         </input>
+        <input name='dataInvoice'
+        type='date'
+        value={this.state.dataInvoice}
+        onChange={this.chandlerInvoice}
+        ></input>
+        </div>
         </>
     ) 
 }
