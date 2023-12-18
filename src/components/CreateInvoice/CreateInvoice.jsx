3a34b35fc64b56f040ed70ea8553a52eab2d6d36
Kacper Lewicki <kacper.lewicki@frista.com>
2023-12-18 18:30:09 +0100

    create a new content

--- src/components/CreateInvoice/CreateInvoice.jsx
+++ src/components/CreateInvoice/CreateInvoice.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./CreateInvoice.css";
 
 class CreateInvoice extends React.Component {
    state = {
@@ -14,31 +15,69 @@ chandlerInvoice = (e) => {
 render(){
     return(
         <>
-        <div>
-        <h1>Create Invoice manual</h1>
-        <input name='nameInvoice' 
+        <h1 className='h1_create_invoices'>Create Invoice manual</h1>
+        <div className='invoices_forms'>
+        <input 
+        className='inputValue_invoices' 
+        name='nameInvoice' 
         type='text' 
         value={this.state.nameInvoice}
         onChange={this.chandlerInvoice} 
-        placeholder='nazwa faktury'>
-        </input>
-        <input name='dataInvoice'
+        placeholder='nazwa faktury'
+        />
+        <input
+        className='inputValue_invoices'
+        name='dataInvoice'
         type='date'
         value={this.state.dataInvoice}
         onChange={this.chandlerInvoice}
-        ></input>
-        <input name='amountInvoice' 
+        />
+        <input
+        className='inputValue_invoices' 
+        name='amountInvoice' 
         type="text"
         value={this.state.amountInvoice}
         onChange={this.chandlerInvoice}
-        placeholder='kwota'>
-        </input>
+        placeholder='kwota'
+        />
+        <input
+        className='inputValue_invoices' 
+        name='status'
+        type='text'
+        value={this.state.status}
+        onChange={this.chandlerInvoice}
+        placeholder='status'
+        />
+        <input
+        className='inputValue_invoices' 
+        name='customer'
+        type='text'
+        value={this.state.customerName}
+        onChange={this.chandlerInvoice}
+        placeholder='customer'
+        />
+        <input
+        className='inputValue_invoices' 
+        name="currency"
+        type='text'
+        value={this.state.currency}
+        onChange={this.chandlerInvoice}
+        placeholder='currency'
+        />
+
+        <button
+        className='invoiceCreateButton'
+        name='InvoiceCreateButton'
+        onClick={this.InvoiceCreateButtonOnChange}>
+        Wyślij fakture
+        </button>
+       
         </div>
+    
         </>
 
     ) 
 }
-
 
 }
 
