1890b9b9854396d36154756a21b465d77c67e39a
Kacper Lewicki <kacper.lewicki@frista.com>
2023-12-23 10:34:55 +0100

    stylistic corrections

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 import  {InvoiceListPage}  from '../pages/invoicesPage/InvoicesPage';
 import NewsPage from '../pages/newsPage/NewsPage';
