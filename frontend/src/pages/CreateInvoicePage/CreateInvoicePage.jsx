8c8290be71fb48418fd6db49b5fab6bab474654c
Kacper Lewicki <kacper.lewicki@frista.com>
2023-12-30 16:34:43 +0100

    adding new content to the project

---
+++ frontend/src/pages/CreateInvoicePage/CreateInvoicePage.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import CreateInvoice from '../../components/CreateInvoice/CreateInvoice';
+import axios from 'axios';
+
+const CreateInvoicePage = () => {
+
+    const invoiceCreateForm = (formData) => {
+
+        const apiEndPoint = "http://localhost:6969/invoice_manualforms";
+
+        axios.post(apiEndPoint,formData)
+        .then(response => {
+            alert('Faktura została pomyślnie utworzona.');
+            console.log(response.data);
+        })
+
+        .catch(error => {
+           console.log ('Wystąpił błąd podczas tworzenia faktury.');
+           console.error(error);
+        })  
+    };
+
+    return (
+        <CreateInvoice onFormSubmit={invoiceCreateForm} />
+
+    )
+}
+
+export default CreateInvoicePage;
