396ffa4ef912e3b375a2c03a6dce4e6a0df7ebc3
Kacper <kacperlewicki.js@gmail.com>
2024-02-23 22:03:41 +0100

    stylistic correction css

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <Link to="/InvoiceListPage"><button 
     className='button_StartMenu'>Invoices List</button></Link> 
     <Link to="/NewsPage"><button 
-    className='button_StartMenu'>News Page</button></Link>
+    className='button_StartMenu'>News</button></Link>
     <Link to="/CreateInvoicePage"><button 
     className='button_StartMenu'>Create Invoice</button></Link>
     </div>
